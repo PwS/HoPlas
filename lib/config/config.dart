c83ef980c7f080d564d4b802255b057cb9e2fdcd
PwS <pawanasutha02@gmail.com>
2021-11-09 10:15:42 +0700

    initial commit

---
+++ lib/config/config.dart
@@ -0,0 +1,10 @@
+class Config {
+  final String appName = 'HoPlas'; //app name
+  final String testerPassword =
+      'tester'; //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken =
+      'AAAA_MQrcTI:APA91bEFzYe2AUlvqTG9MRnjMHqr43PTfYKOZA3qfoIpjVGgacP1U0Zuur77yB4o1M6B4rhCVC-OdMQKcm871EuzRMVPNK2734X6w9H4vTkAMrWHHRDXh3nGcjgLWMT0kiDp4rNObAlo';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
